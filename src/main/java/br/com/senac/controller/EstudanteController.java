b683d90020a55bcf69d5ee20328577261ce0d084
SENACRJEDU\marcelo.estruc <marcelo.estruc@rj.senac.br>
2023-03-14 21:27:51 -0300

    Ajuste do controller

--- src/main/java/br/com/senac/controller/EstudanteController.java
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -3,8 +3,12 @@ package br.com.senac.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +30,39 @@ public class EstudanteController {
 	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
 		return estudanteService.buscarTodosEstudantes();
 	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	
+	@PutMapping
+	public ResponseEntity<Estudante> atualizarEstudante	(@RequestBody Estudante estudante){
+		return estudanteService.atualizarEstudante(estudante);
+	}
+	
+	
+		
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerEstudante(@PathVariable Long id){
+		return estudanteService.removerUsuario(id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
